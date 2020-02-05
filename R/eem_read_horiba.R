32f0746bf5f1121323463f6b897d136a70ba4c27
Joe Shannon <jpshanno@mtu.edu>
2020-02-05 10:13:31 -0500

    Added some generalizability if exported EEM is in a different format

--- R/eem_read_horiba.R
+++ R/eem_read_horiba.R
@@ -6,6 +6,28 @@ eem_read_horiba <- function(file) {
                    stringsAsFactors = FALSE,
                    check.names = FALSE)
 
+  # There a number of export options available for the Horiba CSVs. I haven't
+  # tested them all, but this check should provide some protection if a csv
+  # with different options (eg include comments) is exported rather than a csv
+  # with only the wavelengths and fluorescence intensities
+
+  if(tolower(names(data)[[1]]) != "wavelength"){
+    # Find row and column of fluorescence matrix
+    start_coords <-
+      which(tolower(as.matrix(data)) == "wavelength", arr.ind = TRUE)[1, ]
+
+    # Read in skipping rows above matrix
+    data <-
+      read.csv(file,
+               skip = start_coords[1],
+               stringsAsFactors = FALSE,
+               check.names = FALSE)
+
+    # Remove columns before matrix
+    data <-
+      data[, -c(1:(start_coords[2] - 1))]
+  }
+
   em <- data[["Wavelength"]]
   ex <- as.numeric(names(data)[-1])
 
