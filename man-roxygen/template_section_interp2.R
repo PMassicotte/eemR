6a5256f83e937b6d39da26dd91b223f934b3e1aa
Philippe Massicotte <pmassicotte@hotmail.com>
2025-03-24 07:28:23 -0400

    docs: update RoxygenNote and correct library reference in documentation
    Update RoxygenNote to 7.3.2 for compatibility. Correct the library
    reference from parcma to pracma in documentation to ensure accurate
    linking and user guidance.

--- man-roxygen/template_section_interp2.R
+++ man-roxygen/template_section_interp2.R
@@ -4,9 +4,10 @@
 #'
 #'   Different excitation and emission wavelengths are often used to measure
 #'   EEMs. Hence, it is possible to have mismatchs between measured wavelengths
-#'   and wavelengths used to calculate specific metrics. In these circumstances,
-#'   EEMs are interpolated using the \code{\link{interp2}} function from the
-#'   \code{parcma} library. A message warning the user will be prompted if data
-#'   interpolation is performed.
+#'   and wavelengths used to calculate specific metrics. In these
+#'   circumstances, EEMs are interpolated using the
+#'   \code{\link[pracma]{interp2}} function from the \code{parcma} library. A
+#'   message warning the user will be prompted if data interpolation is
+#'   performed.
 #'
 #' @seealso \link[pracma]{interp2}
